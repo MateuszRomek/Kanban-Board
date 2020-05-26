cead851dc9d7148122870ea5db792bf1f2d892ad
Mateusz <mateuszromek.net@gmail.com>
2020-05-26 22:12:21 +0200

    Adding Redux, React Router

--- src/index.js
+++ src/index.js
@@ -4,11 +4,34 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import SideMenuReducer from './components/SideMenu/duck/reducer';
+import BoardsReducer from './containers/BoardCreator/duck/reducer';
+import { BrowserRouter } from 'react-router-dom';
+
+import { createStore, applyMiddleware, combineReducers, compose } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+
+const rootReducer = combineReducers({
+	SideMenuReducer,
+	BoardsReducer,
+});
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(
+	rootReducer,
+	composeEnhancers(applyMiddleware(thunk))
+);
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+	<Provider store={store}>
+		<BrowserRouter>
+			<React.StrictMode>
+				<App />
+			</React.StrictMode>
+		</BrowserRouter>
+	</Provider>,
+
+	document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
